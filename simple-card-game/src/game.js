578fbddae1b8a9232c10526c463cccbf9cde9198
ygjack <ygjack414@hotmail.com>
2017-12-29 16:35:19 +0800

    update

--- simple-card-game/src/game.js
+++ simple-card-game/src/game.js
@@ -1,7 +1,92 @@
 class Game {
   constructor () {
 
+    this.init()
   }
 
-  
+  init () {
+    this.handleEvents()
+  }
+
+  handleEvents () {
+
+  }
+
+  handlePlayerJoin (player) {
+    this.addPlayer(player)
+
+    if (this.isReadyToStart()) {
+      this.startGame()
+    }
+  }
+
+  handlePlayerLeave (player) {
+    this.removePlayer(player)
+  }
+
+  handlePlayCard (player, card) {
+    this.playCard(player, card)
+
+    if (this.isGameEnd()) {
+      this.endGame()
+    }
+  }
+
+  // check if game should end
+  isGameEnd () {
+
+  }
+
+  // check if game is ready to start
+  isReadyToStart () {
+
+  }
+
+  // add player when "playerjoin"
+  addPlayer (player) {
+
+  }
+
+  // remove player when "playerleft"
+  removePlayer (player) {
+
+  }
+
+  startGame () {
+    this.shuffleCards()
+    this.dealCards()
+  }
+
+  endGame () {
+
+  }
+
+  // shuffle cards before deal
+  shuffleCards () {
+
+  }
+
+  // deal cards to players
+  dealCards () {
+
+  }
+
+  playCard (player, card) {
+
+  }
+
+
 }
+
+// usage would be like
+const game = new Game(roomId)
+
+game.openForJoin()
+
+game.on('playerjoin', () => {})
+
+game.on('playerready', () => {})
+
+game.on('allready', () => {})
+
+game.on('dealing', (player, card) => {})
